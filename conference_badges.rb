cbfb3d415c55a31a02dafaa6f69eb4ddf640d5b9
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-05-28 21:03:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,32 +6,56 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  people = []
-  attendees.each do |badges|
-    people.push ("Hello, my name is #{badges}.")
-
-  #binding.pry
+  
+  attendees.map do |name|
+    badge_maker(name)
   end
-  people
 end
 
 def assign_rooms(attendees)
-  room_assignments = []
-  
-  attendees.each do |people|
-    room_assignments.push ("Hello, #{people}! You'll be assigned to room #{room_assignments.size + 1}!")
+  attendees.map.with_index(1)  do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index}!"
   end
-  room_assignments
 end
 
-def printer(attendees)
-  batch_badges = batch_badge_creator(attendees)
-  rooms = assign_rooms(attendees)
-  batch_badges.each do |paper|
-    puts paper
+def printer(names)
+  batch_badge_creator(names).each do |badge|
+    puts badge
   end
-  rooms.each do |number|
-    puts number
+  assign_rooms(names).each do |assigned_room|
+    puts assigned_room
   end
-  
 end
+
+#def batch_badge_creator(attendees)
+  # people = []
+  # attendees.each do |badges|
+  #   people.push ("Hello, my name is #{badges}.")
+
+  # #binding.pry
+  # end
+  # people
+#end
+
+# def assign_rooms(attendees)
+#   room_assignments = []
+  
+#   attendees.each do |people|
+#     room_assignments.push ("Hello, #{people}! You'll be assigned to room #{room_assignments.size + 1}!")
+#   end
+#   room_assignments
+# end
+
+
+
+# def printer(attendees)
+#   batch_badges = batch_badge_creator(attendees)
+#   rooms = assign_rooms(attendees)
+#   batch_badges.each do |paper|
+#     puts paper
+#   end
+#   rooms.each do |number|
+#     puts number
+#   end
+  
+# end
